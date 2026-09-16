5dabbba2c7b7aebeeaecfff9263d110d02c74337
Pedro PM Dias <59856330+DiasPedroQA@users.noreply.github.com>
2026-09-15 23:42:30 -0300

    feat: Cria pacote utils com logger global e type definitions

---
+++ Atoms/src/utils/__init__.py
@@ -0,0 +1,5 @@
+"""Utilitários gerais da aplicação Neutron Star.
+
+Este pacote fornece ferramentas compartilhadas entre todas as camadas,
+incluindo logging estruturado e definições de tipos específicas.
+"""
